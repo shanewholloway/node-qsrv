2d25847d6ac8df2f3e99a3ce9003eb73355382cb
Shane Holloway <shane.holloway@ieee.org>
2020-03-17 16:18:34 -0600

    Added --watch/-w options and fixed TLS/CERT/KEY argument handling

--- qsrv.cjs
+++ qsrv.cjs
@@ -22,18 +22,20 @@ const qsrv_argv = {
   '--port' (opt, tip) { opt.port = + argv.shift() },
   '--listen' (opt, tip) { opt.listen_addr = argv.shift() },
 
-  '--' (opt, tip, argv) {
-    opt.lsdir = [ ... (opt.lsdir || []), ... argv.splice(0, argv.length) ]
-  },
+  '-w' (opt, tip, argv) { _concat(opt, 'reload', argv.splice(0,1)) },
+  '--watch' (opt, tip, argv) { _concat(opt, 'reload', argv.splice(0,1)) },
+  '--no-reload' (opt, tip, argv) { opt.reload = false },
+
+  '--' (opt, tip, argv) { _concat(opt, 'lsdir', argv.splice(0, argv.length)) },
 
   '--tls' (opt, tip, argv) { assign_creds(opt, argv.shift()) },
   '--cert' (opt, tip, argv) { assign_creds(opt, argv.shift(), 'cert') },
   '--key' (opt, tip, argv) { assign_creds(opt, argv.shift(), 'key') },
 
-  next_arg(opt, tip) {
-    opt.lsdir = [ ... (opt.lsdir || []), tip ]
-  },
+  next_arg(opt, tip) { _concat(opt, 'lsdir', argv.splice(0,1)) },
 }
+
+function _concat(opt, key, rest) { return opt[key] = [... (opt[key] || []), ... rest] }
 
 
 async function qsrv_main(argv = process.argv.slice(2), env = process.env) {
@@ -45,21 +47,41 @@ async function qsrv_main(argv = process.argv.slice(2), env = process.env) {
     ... qsrv_argv.parse(argv) }
 
   if (QSRV_TLS) assign_creds(opt, QSRV_TLS)
-  if (QSRV_TLS_CERT) assign_creds(opt, '.', QSRV_TLS_KEY, 'cert')
+  if (QSRV_TLS_CERT) assign_creds(opt, QSRV_TLS_KEY, 'cert')
   if (QSRV_TLS_KEY) assign_creds(opt, QSRV_TLS_KEY, 'key')
 
   return await qsrv_sevrer(opt)
 }
 
 
-function assign_creds(opt, base, attrs={cert:'cert.pem', key:'key.pem'}) {
-  base = path_resolve(base || '.')
-  attrs = {... attrs}
-  for (const key in attrs)
-    attrs[key] = readFileSync(path_resolve(base, attrs[key]))
+function _readFileOrNull(...args) { try { return readFileSync(...args) } catch (err) { return null } }
+function assign_creds(opt, base, attrs) {
+  const creds = opt.credentials = opt.credentials || {}
 
-  return opt.credentials = Object.assign(
-    {}, opt.credentials, attrs)
+  base = path_resolve(base || '.')
+  if ('string' === typeof attrs) {
+    creds[attrs] = readFileSync(path_resolve(base))
+    return creds
+  }
+
+  if (!attrs && '.' !== base) {
+    // attempt using mkcert.dev format
+    const key_fname = base.replace(/\.pem$/, '') + '-key.pem'
+    const key = _readFileOrNull(key_fname)
+    const cert = key ? _readFileOrNull(base) || _readFileOrNull(base+'.pem') : null
+    if (cert) {
+      creds.key = key
+      creds.cert = cert
+      return creds
+    }
+  }
+
+  // if unset, attempt using cert.pem and key.pem naming convention
+  if (!attrs) attrs = {cert:'cert.pem', key:'key.pem'}
+  for (const key in attrs) {
+    creds[key] = readFileSync(path_resolve(base, attrs[key]))
+  }
+  return opt.credentials = creds
 }
 
 
