c6c3360926316921bd09fca9cdde9bb033c5e3ff
Shane Holloway <shane.holloway@ieee.org>
2020-04-17 09:36:37 -0600

    Moved cli into jsy code

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,36 @@
 import rpi_resolve from '@rollup/plugin-node-resolve'
+import rpi_json from '@rollup/plugin-json'
+import rpi_dgnotify from 'rollup-plugin-dgnotify'
 import rpi_jsy from 'rollup-plugin-jsy'
 
 const configs = []
 export default configs
 
 const sourcemap = true
-const external = ['path', 'fs', 'os', 'dgram', 'https', 'http']
+const external = [
+  'path', 'fs', 'os', 'dgram', 'https', 'http',
+  'qsrv',
+]
+
 const plugins = [
   rpi_resolve({preferBuiltins: true}),
+  rpi_json(),
   rpi_jsy(),
+  rpi_dgnotify(),
 ]
 
 
-configs.push({
-  input: `code/index.jsy`,
-  output: [
-    { file: `esm/index.mjs`, format: 'es', sourcemap },
-    { file: `cjs/index.cjs`, format: 'cjs', exports:'default', sourcemap },
-  ],
-  plugins, external })
-
-
+add_jsy('index', {exports: 'default'})
+add_jsy('cli_qsrv', {exports: 'default'})
 add_jsy('all')
 
 
-function add_jsy(src_name, module_name) {
-  if (!module_name) module_name = src_name
-
+function add_jsy(src_name, opt={}) {
   configs.push({
     input: `code/${src_name}.jsy`,
     output: [
       { file: `esm/${src_name}.mjs`, format: 'es', sourcemap },
-      { file: `cjs/${src_name}.cjs`, format: 'cjs', exports:'named', sourcemap },
+      { file: `cjs/${src_name}.cjs`, format: 'cjs', exports:opt.exports || 'named', sourcemap },
     ],
     plugins, external })
 }
